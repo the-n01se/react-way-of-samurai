d1140ab0c96c25941cfbbf5829d5b556ea1d5af6
Artem <the_n01se@mail.ru>
2021-07-09 19:55:33 +0700

    1

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -18,21 +18,37 @@ const Message = (props) => {
       )
 }
 
+
+
 const Dialogs = (props) => {
+
+      let dialogsData = [
+            { id: 1, name: 'Dimych' },
+            { id: 2, name: 'Andrey' },
+            { id: 3, name: 'Sveta' },
+            { id: 4, name: 'Sasha' },
+            { id: 5, name: 'Viktor' },
+            { id: 6, name: 'Valera' }
+      ]
+
+      let messagesData = [
+            { id: 1, message: 'Hi' },
+            { id: 2, message: 'How is your React?' },
+            { id: 3, message: 'Yo' },
+            { id: 4, message: 'Yo' },
+            { id: 5, message: 'Yo' }
+      ]
+
       return (
             <div className={s.dialogs}>
                   <div className={s.dialogsItems}>
-                        <DialogItem name='Dimych' id='1'/>
-                        <DialogItem name='Andrey' id='2'/>
-                        <DialogItem name='Sveta' id='3'/>
-                        <DialogItem name='Sasha' id='4'/>
-                        <DialogItem name='Viktor' id='5'/>
-                        <DialogItem name='Valera' id='6'/>
+                        <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
                   </div>
                   <div className={s.messages}>
-                        <Message message="Hi"/>
-                        <Message message="How is your React?"/>
-                        <Message message="Yo"/>
+                        <Message message={messagesData[0].message} />
+                        <Message message={messagesData[1].message} />
+                        <Message message={messagesData[2].message} />
                   </div>
             </div>
 
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,13 @@
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
+
 const MyPosts = () => {
+      
+      let postData = [
+            {id: 1, message: 'Hi, how are you?', likesCount: 12},
+            {id: 2, message: 'It\'s my first post', likesCount: 11}
+      ]
+
       return (
             <div className={s.postsBlock}>
                   My posts
@@ -13,8 +20,8 @@ const MyPosts = () => {
                         </div>
                   </div>
                   <div className={s.posts}>
-                        <Post message="Hi, how are you?" likeCounter="15" />
-                        <Post message="It's my first post" likeCounter="27" />
+                        <Post message={postData[0].message} likesCount={postData[0].likesCount} />
+                        <Post message={postData[1].message} likesCount={postData[1].likesCount} />
                   </div>
             </div>
       );
